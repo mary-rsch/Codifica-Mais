66611fbcbe46f4af782784a837b290eca8d93548
Maryana Rigo <maryana.rigo.sch@gmail.com>
2024-05-20 00:09:15 -0300

    Atualizar quebras de linha

--- PHP-CONCEITOS/exercicio_2.php
+++ PHP-CONCEITOS/exercicio_2.php
@@ -7,14 +7,14 @@ $nota2;
 $nota3; 
 $mediaDasNotas;
 
-echo"Digite sua primeira nota: ";
+echo"Digite sua primeira nota: " . PHP_EOL;
 $nota1 = fgets(STDIN);
 
-echo"Digite sua segunda nota: ";
+echo"Digite sua segunda nota: " . PHP_EOL;
 $nota2 = fgets(STDIN);
 
-echo"Digite sua terceira nota: ";
+echo"Digite sua terceira nota: " . PHP_EOL;
 $nota3 = fgets(STDIN);
 
 $mediaDasNotas = ($nota1+$nota2+$nota3) / 3;
-echo "Sua média final é: ",$mediaDasNotas;
+echo "Sua média final é: " . PHP_EOL ,$mediaDasNotas;
