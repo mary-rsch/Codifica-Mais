d5a7fb95b58255045442128412f301e8115ddcc0
Maryana Rigo <maryana.rigo.sch@gmail.com>
2024-07-04 18:27:40 -0300

    Modificar exercicio SQL do dia 27/06

--- SQL-MYSQL/gestao_produtos/criar_banco_produtos.sql
+++ SQL-MYSQL/gestao_produtos/criar_banco_produtos.sql
@@ -5,18 +5,18 @@ use gestao_produtos;
 create table produtos (
     id INT AUTO_INCREMENT primary key,
     nome VARCHAR(255) not null,
-    sku VARCHAR(255) not null,
+    sku VARCHAR(50) unique not null,
     descricao TEXT,
     categoria VARCHAR(255),
     preco DECIMAL(10, 2),
     unidade_medida VARCHAR(50),
     peso DECIMAL(10, 2),
-    quantidade_estoque INT,
+    quantidade_estoque INT default 0,
     fabricante VARCHAR(255),
     fornecedor VARCHAR(255),
-    deleted_at datetime,
-    created_at datetime default CURRENT_TIMESTAMP,
-    updated_at datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+    deleted_at TIMESTAMP null default null,
+    created_at TIMESTAMP default CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 );
 
 select * from produtos;
