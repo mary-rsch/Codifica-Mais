9da8c6ac99d43024986ad00e7cc8b8df8e2b4504
Maryana Rigo <maryana.rigo.sch@gmail.com>
2024-07-25 20:55:47 -0300

    Adicionar Veiculos.php

---
+++ PHP-POO/EXERCICIO-25-7/Veiculos.php
@@ -0,0 +1,15 @@
+<?php
+
+abstract class Veiculo{
+    protected $modelo;
+    protected $marca;
+
+    public function __construct($modelo, $marca) {
+        $this->modelo = $modelo;
+        $this->marca = $marca;
+    }
+}
+
+
+
+
